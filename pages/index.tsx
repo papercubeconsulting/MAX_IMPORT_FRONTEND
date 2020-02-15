580c07adaf08ee80cef089757eade232a09805ef
unknown <chubakueno@gmail.com>
2020-02-14 22:21:39 -0500

    index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,102 +2,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { NextPage } from 'next';
 import React from 'react';
 import { Button } from 'reactstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faEdit, faSearch, faTrashAlt, faEye, faCheck, faCalendarAlt, faUser } from '@fortawesome/free-solid-svg-icons'
-library.add(faEdit, faTrashAlt, faSearch, faEye, faCheck, faCalendarAlt, faUser)
-
 import FieldGroup from '../components/FieldGroup'
-
-type RowStock = {id: number, state: string};
-
-const RowSample: NextPage<{data: RowStock}> = ({data}) => (
-  <tr>
-    <td>
-      <Button color="info" style={{marginRight: 4, marginLeft: 0}}><FontAwesomeIcon icon="edit" /></Button>
-      <Button color="warning" style={{marginRight: 0, marginLeft: 4}}><FontAwesomeIcon icon="trash-alt" /></Button>
-    </td>
-    <td>{data.id}</td>
-    <td>Hangzhou</td>
-    <td>XYZ-456</td>
-    <td>ALM-1 Gim</td>
-    <td>25/11/2019</td>
-    <td>
-      {data.state=='ATE'&&'Atendido'}
-      {data.state=='PEN'&&'Pendiente'}
-    </td>
-    <td>
-      {data.state=='ATE'&&<Button color="info" style={{width: '100%'}}><FontAwesomeIcon icon="eye" /> Ver</Button>}
-      {data.state=='PEN'&&<Button color="success" style={{width: '100%'}}><FontAwesomeIcon icon="check" /> Atender</Button>}
-    </td>
-    <td>Charles X.</td>
-    <td>25/11/2019</td>
-  </tr>
+const Link: NextPage<{href: string,}> = ({href, children}) => (
+    <a style={{flexGrow: 1}} href={href}>
+        <Button color='info' style={{width: '100%', height: '100%', borderRadius: 0}}>
+            {children}
+        </Button>
+    </a>
 )
-const Home: NextPage<{ data: RowStock[] }> = ({ data }) => (
-  <>
-  <>
-  <div className="container" style={{maxWidth: "100%", marginTop: 15}}>
-    <div className="row">
-      <div className="col-sm">
-        <FieldGroup label='Usuario' value='Luis Rivera' icon='user'/>
-      </div>
-      <div className="col-sm">
-        <FieldGroup label='Fecha Inicio' value='01/09/2019' icon='calendar-alt'/>
-      </div>
-      <div className="col-sm">
-        <FieldGroup label='Fecha Fin' value='01/09/2019' icon='calendar-alt'/>
-      </div>
-      <div className="col-sm">
-        <Button color="info"><FontAwesomeIcon icon="search"/> Buscar</Button>
-        <Button color="warning" style={{marginLeft: 10}}>Nuevo abastecimiento</Button>
-      </div>
+const Home: NextPage<{ }> = ({}) => (
+  <div style={{height: '100vh', width: '100vw', display: 'flex'}}>
+    <div style={{flex: 1, flexDirection: "column", display: 'flex'}}>
+        <Link href='#'>Perfil</Link>
+        <Link href='#'>Nueva Proforma</Link>
+        <Link href='#'>Historial Proformas</Link>
+        <Link href='#'>Pagos en Caja</Link>
+        <Link href='#'>Despachos</Link>
+        <Link href='#'>Admin Ventas</Link>
+        <Link href='#'>Inventario</Link>
+        <Link href='./stock'>Abastecimientos</Link>
+        <Link href='#'>BD Clientes</Link>
+        <Link href='#'>Admin Usuarios</Link>
+    </div>
+    <div style={{flex: 3, flexDirection: "column", display: 'flex', margin: 10}}>
+                <FieldGroup label='Fecha' value='01/09/2019' icon='calendar-alt'/>
+                <FieldGroup label='Usuario' value='Luis Rivera' icon='user'/>
+        <div style={{flex: 1, display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
+            <div style={{fontSize: 30, justifyContent: 'center', display: 'flex'}}>
+                Sistema de Gestion Logistica y Venta Max Import
+            </div>
+        </div>
     </div>
   </div>
-  </>
-  <table className="table">
-    <thead className="thead-dark">
-      <tr>
-        <th scope="col">Mtto</th>
-        <th scope="col">Item</th>
-        <th scope="col">Proveedor</th>
-        <th scope="col">Cod. Carga</th>
-        <th scope="col">Almacén</th>
-        <th scope="col">Fecha Reg.</th>
-        <th scope="col">Estatus</th>
-        <th scope="col">Acción</th>
-        <th scope="col">Responsable</th>
-        <th scope="col">Fecha Aten.</th>
-      </tr>
-    </thead>
-    <tbody>
-      {data.map(row=><RowSample data={row}/>)}
-    </tbody>
-  </table>
-  <nav aria-label="Page navigation example">
-    <ul className="pagination justify-content-center">
-      <li className="page-item disabled">
-        <a className="page-link" href="#">Previa</a>
-      </li>
-      <li className="page-item active"><a className="page-link" href="#" tabIndex={-1}>1</a></li>
-      <li className="page-item"><a className="page-link" href="#">2</a></li>
-      <li className="page-item"><a className="page-link" href="#">3</a></li>
-      <li className="page-item"><a className="page-link" href="#">4</a></li>
-      <li className="page-item"><a className="page-link" href="#">5</a></li>
-      <li className="page-item">
-        <a className="page-link" href="#">Siguiente</a>
-      </li>
-    </ul>
-  </nav>
-  </>
 );
-
-Home.getInitialProps = async ({ req }) => {
-  var data = [];
-  for(let i=0;i<10;++i){
-    data.push({id: i+1, state: i<5?'PEN':'ATE'});
-  }
-  return { data };
-};
 
 export default Home;
