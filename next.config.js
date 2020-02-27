525afaa710f135fdccbf18795345e5124dcadd53
Hugo <hufedebe@gmail.com>
2020-02-26 21:08:02 -0500

    add next config

--- next.config.js
+++ next.config.js
@@ -2,3 +2,12 @@ const withCSS = require("@zeit/next-css");
 module.exports = withCSS({
   exportTrailingSlash: true
 });
+
+module.exports = {
+  exportTrailingSlash: true,
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' }
+    };
+  }
+};
