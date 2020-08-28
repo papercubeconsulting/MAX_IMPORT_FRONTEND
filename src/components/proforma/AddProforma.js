e8970140286a80cc1ba04447133730cb4a0f138a
Juan Mendoza <mendoza.jm@pucp.edu.pe>
2020-08-28 16:27:11 -0500

    fix: emergency en modal

--- src/components/proforma/AddProforma.js
+++ src/components/proforma/AddProforma.js
@@ -158,13 +158,13 @@ export const AddProforma = (props) => {
 
         <Grid gridTemplateColumns="repeat(2, 1fr)" gridGap="1rem">
           <Input
-            value={props.totalPaid}
+            value={props.totalPaid/100}
             disabled
             type="number"
             addonBefore="A cuenta S/."
           />
           <Input
-            value={props.totalDebt}
+            value={props.totalDebt/100}
             disabled
             type="number"
             addonBefore="Total deuda S/."
