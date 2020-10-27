5e359f0065b5780a728c38b141a6febf537adfe8
Kiswari <kiswari10@gmail.com>
2020-10-27 16:43:03 -0500

    ✨ add getSale integration and voucher modal v1

--- src/providers/sales.js
+++ src/providers/sales.js
@@ -2,7 +2,8 @@ import { baseProvider } from "./baseProvider";
 
 const postSale = async (body) => baseProvider.httpPost("sales", body);
 const getSales = async (params) => baseProvider.httpGet(`sales`, params);
+const getSale = async (saleId) => baseProvider.httpGet(`sales/${saleId}`);
 const putSale = async (proformaId, body) =>
   baseProvider.httpPut(`sales/${proformaId}/pay`, body);
 
-export { postSale, getSales, putSale };
+export { postSale, getSales, putSale, getSale };
