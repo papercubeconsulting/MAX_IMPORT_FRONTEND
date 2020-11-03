921cc9b65e06572a27db8578f0a55addf6cbcf18
Kiswari <kiswari10@gmail.com>
2020-11-03 15:52:04 -0500

    🎨 add active in sidebar and customers route

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -46,28 +46,36 @@ export const BaseLayout = (props) => {
               <MenuItem>Perfil</MenuItem>
             </Link>
             <Link href="/proforma">
-              <MenuItem>Nueva Proforma</MenuItem>
+              <MenuItem active={isActiveLink("proforma")}>
+                Nueva Proforma
+              </MenuItem>
             </Link>
             <Link href="/proformas">
-              <MenuItem>Historial Proformas</MenuItem>
+              <MenuItem active={isActiveLink("proformas")}>
+                Historial Proformas
+              </MenuItem>
             </Link>
             <Link href="/sales">
-              <MenuItem>Pagos en Caja</MenuItem>
+              <MenuItem active={isActiveLink("sales")}>Pagos en Caja</MenuItem>
             </Link>
             <Link href="/dispatch">
-              <MenuItem>Despachos</MenuItem>
+              <MenuItem active={isActiveLink("dispatch")}>Despachos</MenuItem>
             </Link>
             <Link href="/salesAdministration">
-              <MenuItem active={isActiveLink("salesAdministration")}>Admin Ventas</MenuItem>
+              <MenuItem active={isActiveLink("salesAdministration")}>
+                Admin Ventas
+              </MenuItem>
             </Link>
             <Link href="/products">
               <MenuItem active={isActiveLink("products")}>Inventario</MenuItem>
             </Link>
             <Link href="/supplies">
-              <MenuItem>Abastecimientos</MenuItem>
+              <MenuItem active={isActiveLink("supplies")}>
+                Abastecimientos
+              </MenuItem>
             </Link>
-            <Link href="/">
-              <MenuItem>BD Clientes</MenuItem>
+            <Link href="/customers">
+              <MenuItem active={isActiveLink("customers")}>BD Clientes</MenuItem>
             </Link>
             <Link href="/">
               <MenuItem>Admin Usuarios</MenuItem>
