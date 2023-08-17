399acd2c0e5317153d5523b5d06dd78a50733a84
Reyner Loza <reyner.loza@leniolabs.com>
2023-08-17 14:47:58 -0500

    hot fix

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
-// const serverUrl = process.env.NEXT_PUBLIC_MAXIMPORT_BFF_URL || "/api/";
-const serverUrl = "http://localhost:8080/";
+const serverUrl = process.env.NEXT_PUBLIC_MAXIMPORT_BFF_URL || "/api/";
+// const serverUrl = "http://localhost:8080/";
 export { serverUrl };
