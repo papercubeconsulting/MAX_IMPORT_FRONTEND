1efdc4d46bb4acb9fb420eb2b5d7db6c250a1a46
Reyner Loza <reyner.loza@leniolabs.com>
2023-09-13 15:23:15 -0500

    fix: typo

--- src/pages/proforma/index.js
+++ src/pages/proforma/index.js
@@ -169,7 +169,7 @@ export default ({ setPageTitle }) => {
       dataIndex: "id",
       align: "center",
       render: (id, row) =>
-        `S/ ${' '(
+        `S/ ${(
           get(row, "product.suggestedPrice", 0) * get(row, "quantity", 0)
         ).toFixed(2)}`,
     },
@@ -450,7 +450,6 @@ export default ({ setPageTitle }) => {
     );
     setFinalPrice(newFinalPrice);
   }, [totalPrice, discountPercentage]);
-
 
   useEffect(() => {
     setDiscount(((totalPrice * discountPercentage) / 100).toFixed(2));
